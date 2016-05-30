f27afa67fe4bd162d6cf658a3551d27a2b42b458
qscgy <sam.ehrenstein@gmail.com>
2016-05-29 22:48:22 -0400

    started working on image rotation

--- Blair Walks/RouteViewController.swift
+++ Blair Walks/RouteViewController.swift
@@ -15,12 +15,15 @@ class RouteViewController: UIViewController,UIScrollViewDelegate {
     var end:String!
     var path:[Vertex]!
     var points:[String:CGPoint]!    //every vertex, not just the ones that we're using
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         route=UIImageView(image: UIImage(named: "FullMapRose.png"))
         scrollView=UIScrollView(frame: view.bounds)
         scrollView.contentSize=route.bounds.size
+        scrollView.minimumZoomScale=0.1
+        scrollView.delegate=self
         scrollView.addSubview(route)
         view.addSubview(scrollView)
         
@@ -32,45 +35,20 @@ class RouteViewController: UIViewController,UIScrollViewDelegate {
         drawPath()
     }
     
+    /*
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        route.frame=CGRect(origin: CGPointZero, size: size)
+        scrollView.frame=route.frame
+        scrollView.contentSize=route.frame.size
+        drawPath()
+    }
+    */
+    
+    
     func drawPath(){
-        
-        //print(points)
         let startPt=points[start]
-        //let endPt=points[end]
-        
-        let width:CGFloat=3300
-        let height:CGFloat=2266
-        /*
-        //let scale=view.frame.height/route.image!.size.height
-        let scale=view.frame.height/width
-        print(scale)
-        //let size=CGSizeApplyAffineTransform(route.image!.size, CGAffineTransformMakeScale(scale, scale))
-        let size=CGSizeMake(width, height)
-        
-        UIGraphicsBeginImageContextWithOptions(size, true, 0.0)
-        let context=UIGraphicsGetCurrentContext()
-        //print(context.debugDescription)
-        let xScale:CGFloat=1
-        let yScale:CGFloat=1
-        //route.image!.drawInRect(CGRect(origin: CGPointZero, size: size))
-        //route.image?.drawAtPoint(CGPoint(x: 0, y: 0))
-        
-        CGContextMoveToPoint(context, startPt!.x*xScale, (10-startPt!.y)*yScale)
-        for v:Vertex in path!{
-            if v.name != start!{
-                CGContextAddLineToPoint(context, points[v.name]!.x*xScale, (10-points[v.name]!.y)*yScale)
-            }
-        }
-        
-        CGContextSetStrokeColorWithColor(context, UIColor.redColor().CGColor)
-        CGContextSetLineCap(context, CGLineCap.Round)
-        CGContextSetLineWidth(context, 2.0)
-        CGContextStrokePath(context)
-        route.image=UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        //route.contentMode = .ScaleAspectFit
-        */
+        let width:CGFloat=route.frame.width
+        let height:CGFloat=route.frame.height
         let scale=view.bounds.height/route.image!.size.height
         print(scale)
         //let size=CGSizeApplyAffineTransform(route.image!.size, CGAffineTransformMakeScale(scale, scale))
